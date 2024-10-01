b269fc4509be2f6942e1d03d81ec8a28116b08d8
Md. Ashik Ali Khan <khan.ashik@gmail.com>
2024-10-02 02:08:11 +0600

    JUNIT

--- src/main/java/com/junit/mockitoapp/beans/DemoUtils.java
+++ src/main/java/com/junit/mockitoapp/beans/DemoUtils.java
@@ -6,6 +6,13 @@ public class DemoUtils {
         return num1 + num2;
     }
 
+    public double divide(int num1, int num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("divide by zero");
+        }
+        return (double) num1 / num2;
+    }
+
     public Object checkNull(Object obj) {
         if(obj==null){
             return null;
--- src/test/java/com/junit/mockitoapp/demo/DemoUtilTest.java
+++ src/test/java/com/junit/mockitoapp/demo/DemoUtilTest.java
@@ -3,12 +3,37 @@ package com.junit.mockitoapp.demo;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.junit.mockitoapp.beans.DemoUtils;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class DemoUtilTest {
+
+
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setUp() {
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("@AfterEach");
+    }
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("@BeforeAll");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("@AfterAll");
+    }
+
+
     @Test
-    public void testDemoUtilEqualsOrNotEquals() {
-        DemoUtils demoUtils = new DemoUtils();
+    void testDemoUtilEqualsOrNotEquals() {
         int result = demoUtils.add(10, 20);
         int expected = 30;
         assertEquals(expected, result, "must be 20");
@@ -17,8 +42,13 @@ public class DemoUtilTest {
 
     @Test
     void testNull(){
-        DemoUtils demoUtils = null;
-        assertNull(demoUtils);
+        assertNull( null);
+
+    }
+
+    @Test
+    void testAssertThrows(){
+        assertThrows(ArithmeticException.class, ()->demoUtils.divide(10, 0));
 
     }
 }
